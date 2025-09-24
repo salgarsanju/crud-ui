b5bdeac217a0461c5dca00262da5bc6530de9ed9
salgarsanju@gmail.com <salgarsanju@gmail.com>
2025-09-24 08:09:09 +0530

    formated files

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -4,30 +4,37 @@ import { Observable } from 'rxjs';
 import { Employee } from './employee.mode';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
-
-  constructor(private httpClient: HttpClient) { }
-  api="http://localhost:8080"
-  public saveEmployee(employee:Employee):Observable<Employee>{
-    return this.httpClient.post<Employee>(`${this.api}/save/employee`, employee);
+  constructor(private httpClient: HttpClient) {}
+  api = 'http://localhost:8080';
+  public saveEmployee(employee: Employee): Observable<Employee> {
+    return this.httpClient.post<Employee>(
+      `${this.api}/save/employee`,
+      employee
+    );
   }
-  public getEmployees():Observable<Employee[]>{
+  public getEmployees(): Observable<Employee[]> {
     return this.httpClient.get<Employee[]>(`${this.api}/get/employees`);
   }
 
-  public deleteEmployee(employeeId:number){
-   return this.httpClient.delete<Employee>(`${this.api}/delete/employee/${employeeId}`);
-
+  public deleteEmployee(employeeId: number) {
+    return this.httpClient.delete<Employee>(
+      `${this.api}/delete/employee/${employeeId}`
+    );
   }
 
-  public getEmployee (employeeId:number){
-    return this.httpClient.get<Employee>(`${this.api}/get/employee/${employeeId}`)
+  public getEmployee(employeeId: number) {
+    return this.httpClient.get<Employee>(
+      `${this.api}/get/employee/${employeeId}`
+    );
   }
 
-  public updateEmployee(employee:Employee){
-    return this.httpClient.put<Employee>(`${this.api}/update/employee`,employee);
+  public updateEmployee(employee: Employee) {
+    return this.httpClient.put<Employee>(
+      `${this.api}/update/employee`,
+      employee
+    );
   }
-  
 }
